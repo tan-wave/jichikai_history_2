a78591a16bb3f9d1425fa33c6d7c2d3155984b25
tan-wave <64493750+tan-wave@users.noreply.github.com>
2021-04-07 03:36:44 +0900

    20210407

--- file/figure.js
+++ file/figure.js
@@ -4,39 +4,20 @@ var list_summary = [];
 
 var list_position = -1;//現在位置、進むで+1、戻るで-1、0始まり
 
-var favorite_url = [];
-var favorite_title = [];
-
 function draw() {
 	var html_fig_img = document.getElementById("fig_img");
 	var html_fig_title = document.getElementById("fig_title");
-	var judgment = false;
 
 	//画像データの代入
 	html_fig_img.src = list_url[list_position];
 	html_fig_title.innerHTML = list_summary[list_position];
-
-	//お気に入り登録しているかの判定
-	if (html_fig_img.src != "") {
-		for (i = 0; i < favorite_list_url.length; i++) {
-			if (html_fig_img.src == favorite_url[i]) {
-				judgment = true;
-				break;
-			}
-		}
-		if (judgment) {
-			document.getElementById("favorite_img_star").src = "./icon/star-full.svg";
-		} else {
-			document.getElementById("favorite_img_star").src = "./icon/star-empty.svg";
-		}
-	}
 }
 
 //画像リンククリック時
 function click_img_link(img_original_name) {
 
 	for (let i = 0; i < img_property.length; i++) {
-		if (img_property[i][0].indexOf(img_original_name) !== -1) {	//名前がABとABCであったとき、ABを呼び出せない可能性
+		if (img_property[i][0]==img_original_name) {
 			add_img(i);
 			break;
 		}
@@ -45,7 +26,7 @@ function click_img_link(img_original_name) {
 // ギャラリーページのみ、左右のボタンクリックで移動
 function move_previous(){
 	for (let i = 0; i < img_property.length; i++) {
-		if (img_property[i][2].indexOf(list_title[list_position]) !== -1) {	//名前がABとABCであったとき、ABを呼び出せない可能性
+		if (img_property[i][1]==list_url[list_position]) {
 			if (i>=1){
 				add_img((i-1))
 			}
@@ -55,7 +36,7 @@ function move_previous(){
 }
 function move_following(){
 	for (let i = 0; i < img_property.length; i++) {
-		if (img_property[i][2].indexOf(list_title[list_position]) !== -1) {	//名前がABとABCであったとき、ABを呼び出せない可能性
+		if (img_property[i][1]==list_url[list_position]) {
 			if (i < img_property.length - 1){
 				add_img((i+1))
 			}
@@ -82,11 +63,8 @@ function add_img(i) {
 	list_title.push(img_title);
 	list_summary.push(img_summary);
 
-	// console.log(list_url[0]);
 	draw();
 }
-
-
 
 
 //figcaptureボタンクリック時
@@ -109,36 +87,3 @@ function newpage() {
 		window.open(html_fig_img.src);
 	}
 }
-
-function favorite() {
-	var html_fig_img = document.getElementById("fig_img");
-	var judgment = true;
-	/*初期状態での作動防止*/
-	if (html_fig_img.src != "") {
-		for (i = 0; i < favorite_url.length; i++) {
-			if (html_fig_img.src == favorite_url[i]) {
-				judgment = false;
-				break;
-			}
-		}
-		if (judgment) {
-			favorite_url[favorite_url.length] = html_fig_img.src;
-			favorite_title[favorite_title.length] = html_fig_title.innerHTML;
-			document.getElementById("favorite_img_star").src = "./icon/star-full.svg";
-		} else {
-			favorite_url.splice(i, 1);
-			favorite_title.splice(i, 1);
-			document.getElementById("favorite_img_star").src = "./icon/star-empty.svg";
-		}
-	}
-}
-
-function favorite_list() {
-	var favorite_list = document.getElementById("fig_favorite_list");
-	if (favorite_list.style.display == "none") {
-		favorite_list.style.display = "block";
-	} else {
-		favorite_list.style.display = "none";
-	}
-}
-
--- file/figure_old.js
+++ file/figure_old.js
@@ -1,92 +1,144 @@
-var url = [];
-var title = [];
-var url_po = -1;//現在位置、進むで+1
+var list_url = [];
+var list_title = [];
+var list_summary = [];
+
+var list_position = -1;//現在位置、進むで+1、戻るで-1、0始まり
 
 var favorite_url = [];
 var favorite_title = [];
 
-function draw(){
-	var fig_img = document.getElementById("fig_img");
-	var fig_title = document.getElementById("fig_title");
-	var judgment=false;
-	fig_img.src = url[url_po];
-	fig_title.innerHTML = title[url_po];
-	if(fig_img.src != ""){
-		for(i=0 ; i<favorite_url.length ; i++){
-			if (fig_img.src==favorite_url[i]){
-				judgment=true;
-				break;
-			}
-		}
-		if (judgment){
-			document.getElementById("favorite_img_star").src="./icon/star-full.svg";
-		}else{
-			document.getElementById("favorite_img_star").src="./icon/star-empty.svg";
-		}
-	}
+function draw() {
+	var html_fig_img = document.getElementById("fig_img");
+	var html_fig_title = document.getElementById("fig_title");
+	var judgment = false;
+
+	//画像データの代入
+	html_fig_img.src = list_url[list_position];
+	html_fig_title.innerHTML = list_summary[list_position];
+
+	//お気に入り登録しているかの判定
+	// if (html_fig_img.src != "") {
+	// 	for (i = 0; i < favorite_list_url.length; i++) {
+	// 		if (html_fig_img.src == favorite_url[i]) {
+	// 			judgment = true;
+	// 			break;
+	// 		}
+	// 	}
+	// 	if (judgment) {
+	// 		document.getElementById("favorite_img_star").src = "./icon/star-full.svg";
+	// 	} else {
+	// 		document.getElementById("favorite_img_star").src = "./icon/star-empty.svg";
+	// 	}
+	// }
 }
 
 //画像リンククリック時
-function add_img(url_gen,exp_txt){
-	url.length = url_po + 1;
-	title.length = url_po + 1;
-	url_po ++;
-	url.push("img/"+ url_gen);
-	title.push(exp_txt);
+function click_img_link(img_original_name) {
 
-	draw();
-	
-}
-
-//ボタンクリック時
-function back(){
-	if(url_po > 0){
-		url_po --;
-		draw();
+	for (let i = 0; i < img_property.length; i++) {
+		if (img_property[i][0].indexOf(img_original_name) !== -1) {	//名前がABとABCであったとき、ABを呼び出せない可能性
+			add_img(i);
+			break;
+		}
 	}
 }
-function forward(){
-	if(url.length-1 > url_po){
-		url_po ++;
-		draw();
-	}
-}
-function newpage(){
-	var fig_img = document.getElementById("fig_img");
-	/*初期状態での作動防止*/
-	if(fig_img.src != ""){
-		window.open(fig_img.src);
-	}
-}
-
-function favorite(){
-	var fig_img = document.getElementById("fig_img");
-	var judgment = true;
-	/*初期状態での作動防止*/
-	if(fig_img.src != ""){
-		for(i=0 ; i<favorite_url.length ; i++){
-			if (fig_img.src==favorite_url[i]){
-				judgment=false;
-				break;
+// ギャラリーページのみ、左右のボタンクリックで移動
+function move_previous(){
+	for (let i = 0; i < img_property.length; i++) {
+		if (img_property[i][2].indexOf(list_title[list_position]) !== -1) {	//名前がABとABCであったとき、ABを呼び出せない可能性
+			if (i>=1){
+				add_img((i-1))
 			}
+			break;
 		}
-		if (judgment){
-			favorite_url[favorite_url.length] = fig_img.src;
-			favorite_title[favorite_title.length] = fig_title.innerHTML;
-			document.getElementById("favorite_img_star").src="./icon/star-full.svg";
-		}else{
-			favorite_url.splice(i,1);
-			favorite_title.splice(i,1);
-			document.getElementById("favorite_img_star").src="./icon/star-empty.svg";
+	}
+}
+function move_following(){
+	for (let i = 0; i < img_property.length; i++) {
+		if (img_property[i][2].indexOf(list_title[list_position]) !== -1) {	//名前がABとABCであったとき、ABを呼び出せない可能性
+			if (i < img_property.length - 1){
+				add_img((i+1))
+			}
+			break;
 		}
 	}
 }
 
-function favorite_list(){
-	var favorite_list = document.getElementById("fig_favorite_list");
-	if(favorite_list.style.display == "none"){
-		favorite_list.style.display = "block";
-	}else{
-		favorite_list.style.display = "none";
+//画像の追加（ログの破壊を伴う）
+function add_img(i) {
+
+	let img_url = img_property[i][1];
+	let img_title = img_property[i][2];
+	let img_summary = img_property[i][3];
+
+	// list_position - 1 番目（現在表示している要素）までを残し、残りは破棄する
+	list_position++;
+	list_url.length = list_position;
+	list_title.length = list_position;
+	list_summary.length = list_position;
+
+	// list_position番目に要素を入れる(番号の0がlengthの1と対応するため)
+	list_url.push(img_url);
+	list_title.push(img_title);
+	list_summary.push(img_summary);
+
+	// console.log(list_url[0]);
+	draw();
+}
+
+
+
+
+//figcaptureボタンクリック時
+function back() {
+	if (list_position > 0) {
+		list_position--;
+		draw();
 	}
 }
+function forward() {
+	if (list_url.length - 1 > list_position) {
+		list_position++;
+		draw();
+	}
+}
+function newpage() {
+	var html_fig_img = document.getElementById("fig_img");
+	/*初期状態での作動防止*/
+	if (html_fig_img.src != "") {
+		window.open(html_fig_img.src);
+	}
+}
+
+// function favorite() {
+// 	var html_fig_img = document.getElementById("fig_img");
+// 	var judgment = true;
+// 	/*初期状態での作動防止*/
+// 	if (html_fig_img.src != "") {
+// 		for (i = 0; i < favorite_url.length; i++) {
+// 			if (html_fig_img.src == favorite_url[i]) {
+// 				judgment = false;
+// 				break;
+// 			}
+// 		}
+// 		if (judgment) {
+// 			favorite_url[favorite_url.length] = html_fig_img.src;
+// 			favorite_title[favorite_title.length] = html_fig_title.innerHTML;
+// 			document.getElementById("favorite_img_star").src = "./icon/star-full.svg";
+// 		} else {
+// 			favorite_url.splice(i, 1);
+// 			favorite_title.splice(i, 1);
+// 			document.getElementById("favorite_img_star").src = "./icon/star-empty.svg";
+// 		}
+// 	}
+// }
+
+// function favorite_list() {
+// 	var favorite_list = document.getElementById("fig_favorite_list");
+// 	if (favorite_list.style.display == "none") {
+// 		favorite_list.style.display = "block";
+// 	} else {
+// 		favorite_list.style.display = "none";
+// 	}
+// }
+
